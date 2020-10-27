16d557c6d7cb06eac74123d7384a13e981ad9777
xiegp <1217243450@qq.com>
2020-10-27 17:48:04 +0800

    提交文件

---
+++ HelloGit/src/main/java/com/zhbit/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.zhbit.git;
+
+/**
+ * @author XieGuanPei
+ * @Description
+ * @date 2020/10/27 - 17:42
+ */
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
